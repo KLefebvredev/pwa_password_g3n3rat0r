b47633e2d00d1521d5169a3942d78719ac184c38
klefebvre <klefebvre.dev@gmail.com>
2021-05-10 21:09:22 +0200

    Site V.1

---
+++ js/index.js
@@ -0,0 +1,43 @@
+if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("/serviceWorker.js");
+  }
+  
+
+
+const dataLowercase = "azertyuiopqsdfghjklmwxcvbn";
+const dataUppercase = dataLowercase.toUpperCase();
+const dataNumbers = "0123456789";
+const dataSymbols = "&é'\"(-è_çà)=$ù!:;,*/?.§#{[|`\^@]}¤";
+const rangeValue = document.getElementById('display-password-length');
+const passwordOutput = document.getElementById('password-output');
+
+const generatePassword = () => {
+    let data = [];
+    let password = "";
+
+    if (lowercase.checked) data.push(...dataLowercase);
+    if (uppercase.checked) data.push(...dataUppercase);
+    if (numbers.checked) data.push(...dataNumbers);
+    if (symbols.checked) data.push(...dataSymbols);
+
+    if(data.length === 0) {
+        alert("Veuillez séléctionner des critères");
+        return;
+    }
+
+    for ( i = 0; i < rangeValue.value; i++) { 
+    password +=(data[Math.floor(Math.random() * data.length)]);
+    }
+    passwordOutput.value = password;
+
+    passwordOutput.select();
+    document.execCommand("copy");
+
+    generateButton.textContent ="Copié !";
+        
+    setTimeout(() => {
+        generateButton.textContent="Générer mot de passe";
+    }, 2000);
+};
+
+generateButton.addEventListener("click", generatePassword)
